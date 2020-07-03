b524f11780b172ecc9ac4134766b841f1db26a49
sbstn-jmnz <sbstn.jmnz@yahoo.com>
2020-07-02 21:36:16 -0400

    retrieving courses from API

--- src/components/CoursesGrid.vue
+++ src/components/CoursesGrid.vue
@@ -1,9 +1,38 @@
 <template>
-  <v-container>
-    <h2>Soy el grid</h2>
-  </v-container>
+  <v-row class="mt-8">
+    <v-col cols="12" sm="6" md="4" v-for="course in courses" :key="course.id">
+      <v-card outlined>
+        <v-img class="white--text align-end" height="250px" :src="course.data.img"></v-img>
+        <v-card-text class="pb-0">
+          <p class="pa-0 text-h5 text--primary font-weight-bold">{{course.data.name}}</p>
+          <p class="text--primary text-caption">{{course.data.description}}</p>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            depressed
+            small
+            dark
+            color="amber darken-2 ma-2"
+            :to="{ name: 'course', params:{ name: course.link}}"
+          >Detalles</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-export default {};
+import { mapActions, mapState } from "vuex";
+export default {
+  computed: {
+    ...mapState(["courses"])
+  },
+  methods: {
+    ...mapActions(["getCourses"])
+  },
+  created() {
+    this.getCourses();
+  }
+};
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,28 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
+const baseUrl = 'https://us-central1-livecoding-2800e.cloudfunctions.net/courses'
+
+
 export default new Vuex.Store({
   state: {
+    courses: []
   },
   mutations: {
+    GET_COURSES(state, courses) {
+      state.courses = courses
+    }
   },
   actions: {
+    getCourses({ commit }) {
+      axios.get(`${baseUrl}/courses`)
+        .then(response => {
+          commit('GET_COURSES', response.data)
+        })
+    }
   },
   modules: {
   }
